9d753885ee3f0a6f586602f8f52ffec1189a46f0
vovangy <vovangy@users.noreply.github.com>
2022-11-09 13:28:20 +0000

    deploy: 5c32624ea873645b24090a9d95d8f59474403ad6

--- list_chat-bundle.js
+++ list_chat-bundle.js
@@ -81,31 +81,31 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./list_chat/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./index.css":
-/*!*******************!*\
-  !*** ./index.css ***!
-  \*******************/
+/***/ "./list_chat/index.css":
+/*!*****************************!*\
+  !*** ./list_chat/index.css ***!
+  \*****************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./index.css?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./list_chat/index.css?");
 
 /***/ }),
 
-/***/ "./index.js":
-/*!******************!*\
-  !*** ./index.js ***!
-  \******************/
+/***/ "./list_chat/index.js":
+/*!****************************!*\
+  !*** ./list_chat/index.js ***!
+  \****************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./list_chat/index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\n\nvar form = document.querySelector('form');\nvar input = document.querySelector('.form-input');\nform.addEventListener('submit', handleSubmit.bind(undefined));\nvar chats = localStorage.getItem(\"chats\");\n\nif (!chats) {\n  var current_chats = '{\"1\":{\"user_id\": 1, \"messages\":[{\"user_id\":1,\"value\":\"SIUUUU\",\"time\":\"12:34\"}]}}';\n  localStorage.setItem(\"chats\", current_chats);\n}\n\nvar history = JSON.parse(localStorage.getItem(\"chats\"));\n\nfor (var _i = 0, _Object$keys = Object.keys(history); _i < _Object$keys.length; _i++) {\n  var chat = _Object$keys[_i];\n  var set_chat = new Map();\n  set_chat.set(chat, {\n    \"user_id\": 1,\n    \"messages\": history[chat][\"messages\"]\n  });\n  setChat(set_chat);\n}\n\nfunction handleSubmit(event) {\n  event.preventDefault();\n}\n\nfunction makeChat(chat) {\n  console.log(chat.get(Object.keys(chat)[0]));\n  console.log(chat.get(\"1\")[\"messages\"].slice(-1)[0]);\n  return '          <div class=\"chat-container\" onclick=\"location.href=\\'index.html\\'\"> \\\n    <div class=\"chat-photo\"> \\\n        <img class=\"img-center\" src=\"https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/MESSI.jpg\"></img> \\\n    </div> \\\n    <div class=\"chat-message\"> \\\n        <div class=\"chat-message-name\">' + selectNameFromDatabase(chat.get(\"1\")[\"messages\"].slice(-1)[0][\"user_id\"]) + '</div> \\\n        <div class=\"chat-message-text\">' + chat.get(\"1\")[\"messages\"].slice(-1)[0][\"value\"] + '</div> \\\n    </div> \\\n    <div class=\"chat-time-seen\"> \\\n        <div class=\"chat-time-message\">' + chat.get(\"1\")[\"messages\"].slice(-1)[0][\"time\"] + '</div> \\\n        <div class=\"chat-seen\"> \\\n            <i class=\"material-icons done_all\">done_all</i> \\\n        </div> \\\n    </div> \\\n</div>';\n  return '<a href=\"index.html\"><div>' + selectNameFromDatabase(chat.get(\"1\")[\"messages\"].slice(-1)[0][\"user_id\"]) + '</div>' + chat.get(\"1\")[\"messages\"].slice(-1)[0][\"value\"] + '<div class=\"time\">' + chat.get(\"1\")[\"messages\"].slice(-1)[0][\"time\"] + '</div></div></a>';\n}\n\nfunction setChat(value) {\n  document.getElementById(\"after_end\").insertAdjacentHTML(\"beforeend\", makeChat(value));\n}\n\nfunction selectNameFromDatabase(user_id) {\n  if (user_id == 1) return \"Messi\";\n  return \"Ronaldo\";\n}\n\n//# sourceURL=webpack:///./list_chat/index.js?");
 
 /***/ })
 
