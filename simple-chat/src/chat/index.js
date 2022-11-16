272314acfe69b62bb09812ffbdecf1876a2d9791
Владимир Маленко <vladimirmalenko@192.168.1.5>
2022-11-17 00:05:43 +0300

    Done

--- simple-chat/src/chat/index.js
+++ simple-chat/src/chat/index.js
@@ -1,1 +1,62 @@
 import './index.css';
+
+const form = document.querySelector('form');
+const input = document.querySelector('.form-input');
+form.addEventListener('submit', handleSubmit.bind(form));
+
+var chats = localStorage.getItem("chats");
+var current_chat = localStorage.getItem("current_chat", current_chat);
+
+if (!chats) {
+  chats = '{"1":{"user_id": 1, "messages":[{"user_id":1,"value":"SIUUUUUU","time":"12:34"}]}}';
+  current_chat = "1";
+  localStorage.setItem("chats", chats);
+  localStorage.setItem("current_chat", current_chat);
+}
+
+chats = JSON.parse(chats);
+
+for (let message_cur of chats[current_chat]["messages"]) {
+  let message = new Map();
+  message.set("user_id", Number(message_cur["user_id"]));
+  message.set("value", message_cur["value"]);
+  message.set("time", message_cur["time"]);
+  sendMessage(message);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (!input.value)
+    return;
+  let message = new Map();
+  let date = new Date();
+  message.set("user_id", 0);
+  message.set("value", input.value);
+  message.set("time", date.toLocaleTimeString("ru-Ru").slice(0, 5));
+  addMessageToDict(message);
+  sendMessage(message);
+  input.value = '';
+}
+
+
+function makeMessage(message) {
+  if (message.get("user_id") == 0) {
+    return '<div class = "style-send-message"><div class="sender">' + "Ronaldo" + '</div>' + message.get("value") + '<div class="time">' + message.get("time") + '</div></div>';
+  }
+  return '<div class = "style-get-message"><div class="sender">' + selectNameFromDatabase(message.get("user_id")) + '</div>' + message.get("value") + '<div class="time">' + message.get("time") + '</div></div>';
+}
+
+function addMessageToDict(message) {
+  chats[current_chat]["messages"].push({"user_id":message.get("user_id"), "value":message.get("value"), "time":message.get("time")});
+  localStorage.setItem("chats", JSON.stringify(chats));
+}
+
+function sendMessage(value) {
+  document.getElementById("after_end").insertAdjacentHTML("beforeend", makeMessage(value));
+}
+
+function selectNameFromDatabase(user_id) {
+    if (user_id == 1)
+        return "Messi";
+    return "Белка";
+}
--- simple-chat/src/list_chat/index.js
+++ simple-chat/src/list_chat/index.js
@@ -1,1 +1,80 @@
 import './index.css';
+
+var chats = localStorage.getItem("chats");
+
+if (!chats) {
+  let current_chats = '{"1":{"user_id": 1, "messages":[{"user_id":1,"value":"SIUUUU","time":"12:34"}]}, "2":{"user_id": 2, "messages":[{"user_id":2,"value":"Вопросы?","time":"09:34"}]}}';
+  localStorage.setItem("chats", current_chats);
+}
+var history = JSON.parse(localStorage.getItem("chats"));
+
+for (let chat of Object.keys(history)) {
+    let set_chat = new Map();
+    set_chat.set(chat, {"user_id":history[chat]["user_id"], "messages":history[chat]["messages"]});
+
+    setChat(set_chat, chat);
+
+}
+
+let createChatButton = document.getElementById("chat-create")
+createChatButton.onclick = function() {
+    history = JSON.parse(localStorage.getItem("chats"));
+    let new_chat = String(Number((Object.keys(history).slice(-1))) + 1);
+    let curChats = JSON.parse(localStorage.getItem("chats"));
+    curChats[new_chat] = {"user_id": 1, "messages":[{"user_id":1,"value":"SIUUUU","time":"12:34"}]};
+    localStorage.setItem("chats", JSON.stringify(curChats));
+    let create_chat = new Map();
+    create_chat.set(new_chat, {"user_id":1, "messages":[{"user_id":1,"value":"SIUUUU","time":"12:34"}]})
+    setChat(create_chat, new_chat);
+    let myLink = document.getElementById(new_chat);
+    myLink.onclick = function() {
+        localStorage.setItem("current_chat", new_chat);
+        location.href="chat";
+    }
+    return 1;
+}
+
+for (let chat of Object.keys(history)) {
+    let myLink = document.getElementById(chat);
+    myLink.onclick = function() {
+        localStorage.setItem("current_chat", chat);
+        location.href="chat";
+    }
+}
+
+function makeChat(chat, id) {
+    return '          <div class="chat-container" id=' + id +
+    '><div class="chat-photo"> \
+        <img class="img-center" src=' + selectPhotoFromDatabase(chat.get(id)["user_id"]) + '></img> \
+    </div> \
+    <div class="chat-message"> \
+        <div class="chat-message-name">' + selectNameFromDatabase(chat.get(id)["user_id"]) + '</div> \
+        <div class="chat-message-text">' + chat.get(id)["messages"].slice(-1)[0]["value"] + '</div> \
+    </div> \
+    <div class="chat-time-seen"> \
+        <div class="chat-time-message">' + chat.get(id)["messages"].slice(-1)[0]["time"] +
+        '</div> \
+        <div class="chat-seen"> \
+            <i class="material-icons done_all">done_all</i> \
+        </div> \
+    </div> \
+</div>'
+}
+
+function setChat(value, id) {
+  document.getElementById("after_end").insertAdjacentHTML("beforeend",
+makeChat(value, id));
+    
+}
+
+function selectNameFromDatabase(user_id) {
+    if (user_id == 1)
+        return "Messi";
+    return "Белка";
+}
+
+function selectPhotoFromDatabase(user_id) {
+    if (user_id == 1)
+        return "https://fcb-abj-pre.s3.amazonaws.com/img/jugadors/MESSI.jpg";
+    return "http://goodnewsanimal.ru/_nw/58/80607095.jpg";
+}
